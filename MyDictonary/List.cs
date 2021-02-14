3d9d5d45f71c1c2282a1b029552a406a77d5bd83
SemihKÖKÇÜ <40071340+SemihKokcu@users.noreply.github.com>
2021-02-14 19:47:47 +0300

    Add files via upload

---
+++ MyDictonary/List.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictonary
+{
+    class List<A, T>
+    {
+        A[] keys;
+        T[] items;
+        public List()
+        {
+            keys = new A[0];
+            items = new T[0];
+        }
+        public void Add(A key, T item)
+        {
+            A[] tempArrayKeys = keys;
+            T[] tempArrayItems = items;
+            keys = new A[keys.Length + 1];
+            items = new T[items.Length + 1];
+
+            for (int i = 0; i < tempArrayItems.Length; i++)
+            {
+                keys[i] = tempArrayKeys[i];
+                items[i] = tempArrayItems[i];
+            }
+
+            keys[keys.Length - 1] = key;
+            items[items.Length - 1] = item;
+
+
+        }
+
+        public T[] Items
+        {
+            get { return items; }
+        }
+        public A[] Keys
+        {
+            get { return keys; }
+        }
+
+        public int Lenght
+        {
+            get { return items.Length; }
+        }
+
+        public void Show()
+        {
+            for (int i = 0; i < items.Length; i++)
+            {
+                Console.WriteLine(keys[i] + ": " + items[i]);
+            }
+
+        }
+
+
+
+
+
+
+    }
+}
---
+++ MyDictonary/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace MyDictonary
+{
+    class Program
+    {
+       
+
+        static void Main(string[] args)
+        {
+
+
+            List<int, string> Students = new List<int, string>();
+
+
+            int select = 0;
+            while (select != -1)
+            {
+
+                select = 0;
+
+                Console.WriteLine("Add to Student\n(first data : student number + enter + second data : student name)\n");
+
+                Students.Add((Convert.ToInt32(Console.ReadLine())), (Console.ReadLine()));
+
+                Console.WriteLine("press 1 to continue\nTo Exit : -1\n");
+
+                select = Convert.ToInt32(Console.ReadLine());
+
+
+            }
+
+            Students.Show();
+            
+           
+
+
+
+
+        }
+    }
+}
